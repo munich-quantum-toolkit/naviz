3032c5491736ad0401e398602a5ca644288e4cf1
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-06-03 10:00:42 +0200

    Rever changes to gui assests

--- gui/assets/sw.js
+++ gui/assets/sw.js
@@ -1,33 +1,25 @@
-// Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
-// Copyright (c) 2025 Munich Quantum Software Company GmbH
-// All rights reserved.
-//
-// SPDX-License-Identifier: MIT
-//
-// Licensed under the MIT License
-
-const cacheName = "naviz-pwa";
+const cacheName = 'naviz-pwa';
 const filesToCache = [
-  "./",
-  "./index.html",
-  "./naviz-gui.js",
-  "./naviz-gui_bg.wasm",
+  './',
+  './index.html',
+  './naviz-gui.js',
+  './naviz-gui_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener("install", (e) =>
+self.addEventListener('install', e =>
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
-    }),
-  ),
+    })
+  )
 );
 
 /* Serve cached content when offline */
-self.addEventListener("fetch", (e) =>
+self.addEventListener('fetch', e =>
   e.respondWith(
     caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
-    }),
-  ),
+    })
+  )
 );
